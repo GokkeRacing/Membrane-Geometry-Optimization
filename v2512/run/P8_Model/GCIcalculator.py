66e173e49629f342ad941506898ad2689371e1c1
kasper170103-commits <kasper170103@gmail.com>
2026-03-13 13:36:00 +0100

    Update GCIcalculator.py to correct cell values and enhance installation instructions

--- v2512/run/P8_Model/GCIcalculator.py
+++ v2512/run/P8_Model/GCIcalculator.py
@@ -1,7 +1,10 @@
 from pyGCS import GCS
 
+#Before running this script make sure you have the pyGCS package installed. 
+#You can install it using "pip3 install pyGCS"
+
 # create a grid convergence study object based on a constant domain volume
-gcs = GCS(dimension=3, volume=1, cells=[2208800, 868028, 352530], solution=[0.04992317751946501, 0.04971391283545919, 0.04951542353846154])
+gcs = GCS(dimension=3, volume=1, cells=[2914380, 863280, 255260], solution=[0.04992317751946501, 0.04971391283545919, 0.04951542353846154])
 
 # output information to Markdown-formated table
 gcs.print_table(output_type='markdown', output_path='.')
